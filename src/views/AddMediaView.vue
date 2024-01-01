ad7e9249b16bc76ae5840bf6a223f6c7ae9ea506
BBoranN <92054300+BBoranN@users.noreply.github.com>
2024-01-01 21:02:45 +0300

    fix

--- src/views/AddMediaView.vue
+++ src/views/AddMediaView.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="Container">
-        <input type="text" v-model="mediaTitle" placeholder="Title" />
-        <input type="text" v-model="mediaDescription" placeholder="Description" />
-        <input type="text" v-model="mediaGenre" placeholder="genre">
-        <input type="text" v-model ="mediaDirector" placeholder="director">
-        <input type="text" v-model="mediaPhotoUrl" placeholder="photoUrl">
-
-        <input type="radio" id="movie" value="Movie" v-model="mediaType">
-        <label for="movie">Movie</label>
-        <input type="radio" id="series" value="Series" v-model="mediaType">
-        <label for="series">Series</label>
-
-        <button @click="Add">Add</button>
-        <button @click="Back">Back</button>
+        <div class="InputDiv">
+            <input type="text" v-model="mediaTitle" placeholder="Title" />
+            <input type="text" v-model="mediaDescription" placeholder="Description" />
+            <input type="text" v-model="mediaGenre" placeholder="genre">
+            <input type="text" v-model ="mediaDirector" placeholder="director">
+            <input type="text" v-model="mediaPhotoUrl" placeholder="photoUrl">
+        </div>
+        <div class="Radio">
+            <input type="radio" id="movie" value="Movie" v-model="mediaType">
+            <label for="movie">Movie</label>
+            <input type="radio" id="series" value="Series" v-model="mediaType">
+            <label for="series">Series</label>
+        </div>
+        <div class="AddMediaButtonDiv">
+            <button @click="Add">Add</button>
+            <button @click="Back">Back</button>
+        </div>
     </div>
 </template>
 
@@ -68,4 +72,70 @@ label{
     font-weight: 500;
 }
 
+.Container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    justify-items: center;
+    width: 90%;
+    height: 80%;
+    margin-top: 10%;
+}
+
+input {
+    padding: 10px;
+    padding-right: 0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background: #fff;
+    transition: all 0.3s ease;
+    width: 100%;
+    font-size: 1rem;
+}
+.Radio{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 20%;
+    margin-top: 5%;
+}
+button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    background: #680fbb;
+    color: white;
+    font-size: 1.3rem;
+    font-family: 'Roboto', sans-serif;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    width: 100%;
+}
+
+button:hover {
+    background: #680fbb;
+}
+
+button:active {
+    background: #004085;
+}
+.AddMediaButtonDiv{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 20%;
+    margin-top: 5%;
+}
+.InputDiv{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    width: 60%;
+    margin-top: 5%;
+}
 </style>
